f65793c1d203a4375ab64c1139a0a318a5e9bd56
shellytoons <exeleo652@gmail.com>
2025-06-17 10:29:56 -0300

    Add files via upload

---
+++ services/firebaseApp.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGTzlFgyw9duT-SchnUEuEJkzaknkmOXM",
+  authDomain: "vaporeon-6f726.firebaseapp.com",
+  projectId: "vaporeon-6f726",
+  storageBucket: "vaporeon-6f726.firebasestorage.app",
+  messagingSenderId: "994852373536",
+  appId: "1:994852373536:web:fbbbe55742dc6298082845"
+};
+
+const firesabeApp = initializeApp(firebaseConfig); 
+
+export default firesabeApp; 
